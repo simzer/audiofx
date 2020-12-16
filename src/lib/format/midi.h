5c06bba471236ce812723ea7ffbeb382379e32c3
Laszlo Simon <laszlo.simon@gmail.com>
2020-12-16 18:04:59 +0100

    midi header

--- src/lib/format/midi.h
+++ src/lib/format/midi.h
@@ -12,11 +12,19 @@ namespace format
 namespace midi
 {
 
+static constexpr uint32_t chunkType(const char *s) {
+    return (uint32_t)s[0] | (s[1] << 8u) | (s[2] << 16u) | (s[3] << 24u);
+}
+
 #pragma pack(push, 1)
 
 struct Chunk
 {
-    typedef char Type[4];
+    enum class Type : uint32_t {
+        header = chunkType("MThd"),
+        track = chunkType("MTrk")
+    };
+
     Type type;
     math::beuint32 dataLength;
     char *begin() { return (char*)(this + 1); }
@@ -25,6 +33,19 @@ struct Chunk
 };
 
 static_assert (sizeof(Chunk) == 8, "Chunk size missmatch");
+
+struct HeaderData
+{
+    enum class Format : uint16_t {
+        single = 0 << 8,
+        simultaneous = 1 << 8,
+        sequential = 2 << 8
+    };
+
+    Format format;
+    math::beuint16 numOfTracks;
+    math::beuint16 division;
+};
 
 #pragma pack(pop)
 
